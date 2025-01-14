499b3bad4d37138e9d88d479d5fb2640c5b24f00
Dan Rusnac <rusnac14@gmail.com>
2025-01-14 10:59:41 +0100

    fix: animation

--- packages/vechain-kit/src/components/common/Motion/FadeInViewFromBottom.tsx
+++ packages/vechain-kit/src/components/common/Motion/FadeInViewFromBottom.tsx
@@ -1,16 +1,27 @@
 import { motion } from 'framer-motion';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 
 type Props = {
     children: ReactNode;
 };
 
 export const FadeInViewFromBottom = ({ children }: Props) => {
+    // Prevent scroll position from affecting animation
+    useEffect(() => {
+        const modalContent = document.querySelector('.chakra-modal__content');
+        if (modalContent) {
+            modalContent.scrollTop = 0;
+        }
+    }, []);
+
     return (
         <motion.div
-            initial={{ opacity: 1, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.25, easing: [0.17, 0.55, 0.55, 1] }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+                duration: 0.2,
+                ease: 'easeOut',
+            }}
         >
             {children}
         </motion.div>
