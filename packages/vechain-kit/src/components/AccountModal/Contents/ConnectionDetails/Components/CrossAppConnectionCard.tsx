1986f461603133552a6ed1ebf32607b857f3c44e
Dan Rusnac <rusnac14@gmail.com>
2025-01-23 11:09:43 +0100

    fix: crossapp connection details

--- packages/vechain-kit/src/components/AccountModal/Contents/ConnectionDetails/Components/CrossAppConnectionCard.tsx
+++ packages/vechain-kit/src/components/AccountModal/Contents/ConnectionDetails/Components/CrossAppConnectionCard.tsx
@@ -1,4 +1,4 @@
-import { Text, HStack } from '@chakra-ui/react';
+import { Text, HStack, VStack } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { CrossAppConnectionCache } from '@/types';
 import { useVeChainKitConfig } from '@/providers';
@@ -14,22 +14,45 @@ export const CrossAppConnectionCard = ({ connectionCache }: Props) => {
     return (
         <>
             {connectionCache.ecosystemApp && (
-                <HStack
+                <VStack
                     p={4}
                     bg={isDark ? '#1a1a1a' : '#f5f5f5'}
                     borderRadius={'xl'}
                     spacing={4}
                     w="full"
-                    justifyContent="space-between"
                 >
-                    <Text fontSize="sm" color={isDark ? '#dfdfdd' : '#4d4d4d'}>
-                        {t('Connected through')}:
-                    </Text>
+                    <HStack w="full" justifyContent="space-between">
+                        <Text
+                            fontSize="sm"
+                            color={isDark ? '#dfdfdd' : '#4d4d4d'}
+                        >
+                            {t('Connected through')}:
+                        </Text>
+                        <Text
+                            fontSize="sm"
+                            color={isDark ? '#dfdfdd' : '#4d4d4d'}
+                        >
+                            {connectionCache.ecosystemApp.name}
+                        </Text>
+                    </HStack>
 
-                    <Text fontSize="sm" color={isDark ? '#dfdfdd' : '#4d4d4d'}>
-                        {new Date(connectionCache.timestamp).toLocaleString()}
-                    </Text>
-                </HStack>
+                    <HStack w="full" justifyContent="space-between">
+                        <Text
+                            fontSize="sm"
+                            color={isDark ? '#dfdfdd' : '#4d4d4d'}
+                        >
+                            {t('Connected at')}:
+                        </Text>
+                        <Text
+                            fontSize="sm"
+                            color={isDark ? '#dfdfdd' : '#4d4d4d'}
+                        >
+                            {new Date(
+                                connectionCache.timestamp,
+                            ).toLocaleString()}
+                        </Text>
+                    </HStack>
+                </VStack>
             )}
         </>
     );
