2d2abe737e2fb8b0052416f21b86e14a3bd51175
Dan Rusnac <rusnac14@gmail.com>
2025-01-26 01:05:06 +0100

    fix: metadata

--- examples/sample-next-vechain-app/src/app/layout.tsx
+++ examples/sample-next-vechain-app/src/app/layout.tsx
@@ -20,39 +20,60 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning={true}>
+        <html
+            lang="en"
+            suppressHydrationWarning={true}
+            style={{
+                scrollBehavior: 'smooth',
+            }}
+        >
             <head>
-                <link
-                    rel="icon"
-                    href="/images/vechain-kit-long.svg"
-                    type="image/svg+xml"
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-
                 <title>VeChain Kit</title>
                 <meta
                     name="description"
                     content="VeChain Kit - A powerful and intuitive toolkit for building and interacting with decentralized applications on VeChain blockchain."
                 />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <link
+                    rel="icon"
+                    href="/images/vechain-kit-long.svg"
+                    type="image/svg+xml"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="57x57"
+                    href="/images/vechain-kit-long.svg"
+                />
+                <meta
+                    name="msapplication-TileImage"
+                    content="/images/vechain-kit-long.svg"
+                />
 
+                {/* Open Graph Metadata */}
+                <meta name="title" property="og:title" content="VeChain Kit" />
+                <meta name="type" property="og:type" content="website" />
                 <meta
                     property="og:url"
                     content="https://sample-vechain-app-demo.vechain.org/"
                 />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="VeChain Kit" />
                 <meta
                     property="og:description"
                     content="VeChain Kit - A powerful and intuitive toolkit for building and interacting with decentralized applications on VeChain blockchain."
                 />
+                <meta property="og:site_name" content="VeChain Kit" />
                 <meta
                     property="og:image"
                     content="/images/vechain-kit-long.svg"
                 />
+                <meta property="og:image:type" content="image/svg+xml" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="og:image:alt" content="VeChain Kit" />
 
+                {/* Twitter Metadata */}
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="VeChain Kit" />
                 <meta
@@ -63,6 +84,7 @@ export default function RootLayout({
                     name="twitter:image"
                     content="/images/vechain-kit-long.svg"
                 />
+                <meta name="twitter:image:alt" content="VeChain Kit" />
             </head>
             <body>
                 {/* Chakra UI Provider */}
