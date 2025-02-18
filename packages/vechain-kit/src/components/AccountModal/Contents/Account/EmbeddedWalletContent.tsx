bad7ad4b75b03f3dc6c2803ad8f2e4391cb26e43
Dan Rusnac <rusnac14@gmail.com>
2025-02-18 18:01:42 +0100

    style: access and security contents

--- packages/vechain-kit/src/components/AccountModal/Contents/Account/EmbeddedWalletContent.tsx
+++ packages/vechain-kit/src/components/AccountModal/Contents/Account/EmbeddedWalletContent.tsx
@@ -4,11 +4,10 @@ import {
     ScrollToTopWrapper,
     StickyHeaderContainer,
 } from '@/components/common';
-import { useCrossAppConnectionCache, usePrivy, useWallet } from '@/hooks';
+import { useCrossAppConnectionCache, useWallet } from '@/hooks';
 import { useVeChainKitConfig } from '@/providers';
 import { getPicassoImage } from '@/utils';
 import {
-    Button,
     Divider,
     Icon,
     Image,
@@ -20,13 +19,10 @@ import {
     Text,
     VStack,
 } from '@chakra-ui/react';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AccountModalContentTypes } from '../../Types';
 import { IoOpenOutline } from 'react-icons/io5';
 import { WalletSecuredBy } from '../ConnectionDetails/Components';
-import { ActionButton } from '../../Components';
-import { GiHouseKeys } from 'react-icons/gi';
 
 type Props = {
     setCurrentContent: (content: AccountModalContentTypes) => void;
@@ -35,13 +31,10 @@ type Props = {
 export const EmbeddedWalletContent = ({ setCurrentContent }: Props) => {
     const { t } = useTranslation();
     const { connectedWallet, connection } = useWallet();
-    const [showFullText, setShowFullText] = useState(false);
     const walletImage = getPicassoImage(connectedWallet?.address ?? '');
     const { getConnectionCache } = useCrossAppConnectionCache();
     const connectionCache = getConnectionCache();
     const { darkMode: isDark } = useVeChainKitConfig();
-
-    const { exportWallet } = usePrivy();
 
     return (
         <ScrollToTopWrapper>
@@ -74,7 +67,10 @@ export const EmbeddedWalletContent = ({ setCurrentContent }: Props) => {
                             maxW={'100px'}
                             borderRadius="50%"
                         />
-                        <AddressDisplay wallet={connectedWallet} />
+                        <AddressDisplay
+                            wallet={connectedWallet}
+                            style={{ mt: 2 }}
+                        />
                     </VStack>
 
                     {connection.isConnectedWithCrossApp && (
@@ -89,31 +85,16 @@ export const EmbeddedWalletContent = ({ setCurrentContent }: Props) => {
                                 )}
                             </Text>
 
-                            {showFullText && (
-                                <>
-                                    <Text fontSize={'sm'} opacity={0.5}>
-                                        {t(
-                                            'This wallet is the owner of your smart account, which is used as your identity and as a gateway for your blockchain interactions.',
-                                        )}
-                                    </Text>
-                                    <Text fontSize={'sm'} opacity={0.5}>
-                                        {t(
-                                            'Please be sure to keep this wallet safe and backed up.',
-                                        )}
-                                    </Text>
-                                </>
-                            )}
-
-                            <Button
-                                mt={0}
-                                variant="link"
-                                size="sm"
-                                onClick={() => setShowFullText(!showFullText)}
-                                color="blue.500"
-                                textAlign="left"
-                            >
-                                {t(showFullText ? 'Show Less' : 'Read More')}
-                            </Button>
+                            <Text fontSize={'sm'} opacity={0.5}>
+                                {t(
+                                    'This wallet is the owner of your smart account, which is used as your identity and as a gateway for your blockchain interactions.',
+                                )}
+                            </Text>
+                            <Text fontSize={'sm'} opacity={0.5}>
+                                {t(
+                                    'Please be sure to keep this wallet safe and backed up.',
+                                )}
+                            </Text>
                         </>
                     )}
 
@@ -125,75 +106,43 @@ export const EmbeddedWalletContent = ({ setCurrentContent }: Props) => {
                                 )}
                             </Text>
 
-                            {showFullText && (
-                                <>
-                                    <Text fontSize={'sm'} opacity={0.5}>
-                                        {t(
-                                            'This wallet is the owner of your smart account, which is used as your identity and as a gateway for your blockchain interactions.',
-                                        )}
-                                    </Text>
-                                    <Text fontSize={'sm'} opacity={0.5}>
-                                        {t(
-                                            'We highly recommend exporting your private key to back up your wallet. This ensures you can restore it if needed or transfer it to self-custody using',
-                                        )}
-                                        <Link
-                                            href="https://www.veworld.net/"
-                                            isExternal
-                                            color="gray.500"
-                                            fontSize={'14px'}
-                                            textDecoration={'underline'}
-                                        >
-                                            {' '}
-                                            {t('VeWorld Wallet')}
-                                            <Icon ml={1} as={IoOpenOutline} />
-                                        </Link>
-                                        .
-                                    </Text>
-                                    <Text fontSize={'sm'} opacity={0.5}>
-                                        {t('Click')}{' '}
-                                        <Link
-                                            href="https://docs.vechain-kit.vechain.org/vechain-kit/embedded-wallets"
-                                            isExternal
-                                            color="gray.500"
-                                            fontSize={'14px'}
-                                            textDecoration={'underline'}
-                                        >
-                                            {t('here')}
-                                        </Link>{' '}
-                                        {t(
-                                            'to learn more about embedded wallets.',
-                                        )}
-                                    </Text>
-                                </>
-                            )}
-
-                            <Button
-                                mt={0}
-                                variant="link"
-                                size="sm"
-                                onClick={() => setShowFullText(!showFullText)}
-                                color="blue.500"
-                                textAlign="left"
-                            >
-                                {t(showFullText ? 'Show Less' : 'Read More')}
-                            </Button>
+                            <Text fontSize={'sm'} opacity={0.5}>
+                                {t(
+                                    'This wallet is the owner of your smart account, which is used as your identity and as a gateway for your blockchain interactions.',
+                                )}
+                            </Text>
+                            <Text fontSize={'sm'} opacity={0.5}>
+                                {t(
+                                    'We highly recommend exporting your private key to back up your wallet. This ensures you can restore it if needed or transfer it to self-custody using',
+                                )}
+                                <Link
+                                    href="https://www.veworld.net/"
+                                    isExternal
+                                    color="gray.500"
+                                    fontSize={'14px'}
+                                    textDecoration={'underline'}
+                                >
+                                    {' '}
+                                    {t('VeWorld Wallet')}
+                                    <Icon ml={1} as={IoOpenOutline} />
+                                </Link>
+                                .
+                            </Text>
+                            <Text fontSize={'sm'} opacity={0.5}>
+                                {t('Click')}{' '}
+                                <Link
+                                    href="https://docs.vechain-kit.vechain.org/vechain-kit/embedded-wallets"
+                                    isExternal
+                                    color="gray.500"
+                                    fontSize={'14px'}
+                                    textDecoration={'underline'}
+                                >
+                                    {t('here')}
+                                </Link>{' '}
+                                {t('to learn more about embedded wallets.')}
+                            </Text>
                         </>
                     )}
-
-                    <ActionButton
-                        title={t('Backup your wallet')}
-                        description={t(
-                            connection.isConnectedWithSocialLogin
-                                ? 'Store your Recovery Phrase or Private Key in a secure location, avoid losing access to your assets.'
-                                : 'Backup can be done only in the app securing your wallet.',
-                        )}
-                        onClick={() => {
-                            exportWallet();
-                        }}
-                        isDisabled={!connection.isConnectedWithSocialLogin}
-                        leftIcon={GiHouseKeys}
-                        // rightIcon={MdOutlineNavigateNext}
-                    />
                 </VStack>
             </ModalBody>
             <ModalFooter w={'full'}>
