3805e9a3caf683c981c4210f4f59dad6313be2a2
Dan Rusnac <rusnac14@gmail.com>
2025-03-05 09:44:30 +0100

    fix: style

--- examples/homepage/src/app/layout.tsx
+++ examples/homepage/src/app/layout.tsx
@@ -26,6 +26,8 @@ function AppContent({ children }: { children: React.ReactNode }) {
     return (
         <div
             style={{
+                width: '100%',
+                height: '100%',
                 backgroundColor: bgColor,
             }}
         >
