484f8da791d5bd1fe71e58456839889dcc813a1d
Jennifer E <echenimjennifer@gmail.com>
2025-03-11 04:12:26 +0400

    feat: add new VeBetter logo and icon components for light and dark modes

--- packages/vechain-kit/src/assets/icons/VechainLogo/index.ts
+++ packages/vechain-kit/src/assets/icons/VechainLogo/index.ts
@@ -5,3 +5,11 @@ export * from './VechainLogoLight';
 export * from './VechainIcon';
 export * from './VechainIconLight';
 export * from './VechainIconDark';
+
+export * from './VeBetterLogo';
+export * from './VeBetterLogoLight';
+export * from './VeBetterLogoDark';
+
+export * from './VeBetterIcon';
+export * from './VeBetterIconLight';
+export * from './VeBetterIconDark';
