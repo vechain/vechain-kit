b0e581b27a402c39c6997ec4ffdefd98d96f6da8
Victor Emanuel <versaujob@gmail.com>
2025-03-17 19:19:39 +0000

    refactor: update connected wallet and wallet display components to support small devices

--- packages/vechain-kit/src/components/WalletButton/ConnectedWallet.tsx
+++ packages/vechain-kit/src/components/WalletButton/ConnectedWallet.tsx
@@ -1,8 +1,9 @@
-import { Button, HStack, useMediaQuery } from '@chakra-ui/react';
 import { useWallet } from '@/hooks';
-import { WalletDisplay } from './WalletDisplay';
-import { WalletButtonProps } from './WalletButton';
+import { Button, HStack, useMediaQuery } from '@chakra-ui/react';
+
 import { AccountAvatar } from '../common';
+import { WalletButtonProps } from './WalletButton';
+import { WalletDisplay } from './WalletDisplay';
 
 type ConnectedWalletProps = WalletButtonProps & {
     onOpen: () => void;
@@ -18,8 +19,14 @@ export const ConnectedWallet = ({
     const [isDesktop] = useMediaQuery('(min-width: 768px)');
 
     return (
-        <Button {...buttonStyle} onClick={onOpen} p={'0px 13px'} minH={'45px'}>
-            <HStack>
+        <Button
+            {...buttonStyle}
+            onClick={onOpen}
+            w="full"
+            minH={'45px'}
+            maxW="fit-content"
+        >
+            <HStack w="full" px={2} minW="fit-content">
                 <AccountAvatar
                     wallet={account}
                     props={{
