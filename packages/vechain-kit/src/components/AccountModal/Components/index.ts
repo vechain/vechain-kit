6b357ce85d6fe63b8a9b0e63fdbc3f94d1505e77
Michael Redmond <michael.redmond@vechain.org>
2025-04-25 17:20:45 +0100

    Update language file and remove both AssetsSection and AssetsTabPanel

--- packages/vechain-kit/src/components/AccountModal/Components/index.ts
+++ packages/vechain-kit/src/components/AccountModal/Components/index.ts
@@ -2,7 +2,6 @@ export * from './AccountDetailsButton';
 export * from './ActionButton';
 export * from './AccountSelector';
 export * from './BalanceSection';
-export * from './Tabs/AssetsSection';
 export * from './QuickActionsSection';
 export * from './Alerts';
 export * from './CrossAppConnectionSecurityCard';
