3776ff4e3d9b5688dd081f3dd38e363aed7cb53a
Alexander K <alexander.khristich@gmail.com>
2025-05-07 23:39:53 +0300

    assert session deletion after logout

--- tests/e2e/tests/connect-wallet.spec.ts
+++ tests/e2e/tests/connect-wallet.spec.ts
@@ -3,7 +3,7 @@ import { expect } from "@playwright/test"
 import { veWorldMockClient } from "@vechain/veworld-mock-playwright"
 import { HomePage } from "../models/HomePage";
 import { DashboardPage } from "../models/DashboardPage";
-import {DENIAL_KITCHEN, PRIVY_TEST_EMAIL_SENDER} from "../constants";
+import {DENIAL_KITCHEN, HOMEPAGE, PRIVY_TEST_EMAIL_SENDER} from "../constants";
 import { trimAddress } from "../utils/strings";
 import {AccountModal} from "../models/AccountModal";
 
@@ -18,7 +18,7 @@ test.describe("Connect Wallet", () => {
         accountModal = new AccountModal(page, context, veWorldMockClient)
     })
 
-    test('Can log in using VeWorld wallet and then log out', async () => {
+    test('Can log in using VeWorld wallet and then log out', async ({ page }) => {
         await homePage.open()
         await homePage.initVWMock(0)
         await homePage.connectWallet()
@@ -26,6 +26,8 @@ test.describe("Connect Wallet", () => {
         await dashboardPage.openAccountModal()
         await accountModal.logOut()
         await expect(homePage.loginButton).toBeVisible()
+        const storagePostLogout = await page.evaluate(() => { return localStorage });
+        expect(storagePostLogout).not.toHaveProperty("dappkit@vechain/connectionCertificate")
     })
 })
 
@@ -40,12 +42,16 @@ test.describe.skip("Privy", () => {
         accountModal = new AccountModal(page, context, veWorldMockClient)
     })
 
-    test('Can log in using email and then log out', async () => {
+    test('Can log in using email and then log out', async ({ page }) => {
         await homePage.open()
         await homePage.loginWithEmail(PRIVY_TEST_EMAIL_SENDER)
         await expect(dashboardPage.walletButton).toBeVisible()
         await dashboardPage.openAccountModal()
         await accountModal.logOut()
         await expect(homePage.loginButton).toBeVisible()
+        const isSessionDeleted = await page.evaluate(() =>
+            Object.keys(localStorage).some(key => key.includes('privy_wallet:'))
+        );
+        expect(isSessionDeleted).toBeTruthy()
     })
 })
