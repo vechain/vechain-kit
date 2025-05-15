ea1b3e02641e6bd99af1b752b5ac735bd828e30f
Victor Emanuel <versaujob@gmail.com>
2025-05-15 14:28:01 +0100

    fix: close on overlay

--- packages/vechain-kit/src/components/AccountModal/AccountModal.tsx
+++ packages/vechain-kit/src/components/AccountModal/AccountModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useWallet, useNotificationAlerts } from '@/hooks';
 import { BaseModal } from '@/components/common';
 import {
@@ -57,14 +57,6 @@ export const AccountModal = ({
         accountModalContent: currentContent,
         setAccountModalContent: setCurrentContent,
     } = useModal();
-
-    //Disable close on overlay for all content except disconnect confirm
-    const canCloseOnOverlayClick = useMemo(() => {
-        return (
-            typeof currentContent === 'object' &&
-            currentContent.type !== 'disconnect-confirm'
-        );
-    }, [currentContent]);
 
     useEffect(() => {
         if (isOpen && initialContent) {
@@ -255,7 +247,6 @@ export const AccountModal = ({
             onClose={onClose}
             allowExternalFocus={true}
             blockScrollOnMount={true}
-            closeOnOverlayClick={canCloseOnOverlayClick}
         >
             {renderContent()}
         </BaseModal>
