3add0da0392a8a88632248e0f0eaee7c5b01682c
Berk Akgun <akgunberk@yahoo.com>
2025-05-26 16:49:47 +0300

    chore: export useThor from dapp-kit

--- packages/vechain-kit/src/hooks/index.ts
+++ packages/vechain-kit/src/hooks/index.ts
@@ -4,6 +4,7 @@ export {
   useSetWalletRecovery,
 } from '@privy-io/react-auth';
 export {
+  useThor,
   useWallet as useDAppKitWallet,
   useWalletModal as useDAppKitWalletModal,
 } from '@vechain/dapp-kit-react';
