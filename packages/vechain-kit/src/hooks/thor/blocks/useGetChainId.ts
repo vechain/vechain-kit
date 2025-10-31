19bb03cd7bf7c7c4fb08a13fb7c1837779e0fab1
Michael Redmond <michael.redmond@vechain.org>
2025-10-31 16:09:49 +0000

    remove refetch from useGetChainId

--- packages/vechain-kit/src/hooks/thor/blocks/useGetChainId.ts
+++ packages/vechain-kit/src/hooks/thor/blocks/useGetChainId.ts
@@ -22,6 +22,5 @@ export const useGetChainId = () => {
         queryKey: getChainIdQueryKey(),
         queryFn: () => getChainId(thor),
         enabled: !!thor,
-        refetchInterval: 10000,
     });
 };
