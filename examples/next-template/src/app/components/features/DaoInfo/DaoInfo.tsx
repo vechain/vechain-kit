34933b1f175a56932f48894f2541255618590850
Dan Rusnac <rusnac14@gmail.com>
2025-12-01 11:48:38 +0100

    fix: dao info error

--- examples/next-template/src/app/components/features/DaoInfo/DaoInfo.tsx
+++ examples/next-template/src/app/components/features/DaoInfo/DaoInfo.tsx
@@ -4,19 +4,15 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 import {
     useWallet,
     useCurrentAllocationsRoundId,
-    useSelectedGmNft,
     useParticipatedInGovernance,
-    useIsPerson,
 } from '@vechain/vechain-kit';
 
 export function DaoInfo() {
     const { account } = useWallet();
     const { data: currentAllocationsRoundId } = useCurrentAllocationsRoundId();
-    const { gmId } = useSelectedGmNft(account?.address ?? '');
     const { data: participatedInGovernance } = useParticipatedInGovernance(
         account?.address ?? '',
     );
-    const { data: isValidPassport } = useIsPerson(account?.address);
 
     return (
         <Box>
@@ -24,15 +20,9 @@ export function DaoInfo() {
             <Text data-testid="current-allocation-round-id">
                 Current Allocations Round ID: {currentAllocationsRoundId}
             </Text>
-            <Text data-testid="selected-gm-nft">
-                Selected GM NFT: {gmId === '0' ? 'None' : gmId}
-            </Text>
             <Text data-testid="participated-in-governance">
                 Participated in Governance:{' '}
                 {participatedInGovernance?.toString()}
-            </Text>
-            <Text data-testid="is-passport-valid">
-                Is Passport Valid: {isValidPassport?.toString()}
             </Text>
         </Box>
     );
