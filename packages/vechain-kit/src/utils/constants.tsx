04981908b6e245d7d8fe209be312a41c68004d04
Victor Emanuel <versaujob@gmail.com>
2025-12-10 10:33:53 +0000

    fix: remove token logo and component constants

--- packages/vechain-kit/src/utils/constants.tsx
+++ packages/vechain-kit/src/utils/constants.tsx
@@ -9,22 +9,7 @@ import {
 } from '@/types/gasToken';
 import { getConfig } from '@/config';
 
-export const TOKEN_LOGOS: Record<string, string> = {
-    VET: 'https://cryptologos.cc/logos/vechain-vet-logo.png',
-    VTHO: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3012.png',
-    B3TR: 'https://vechain.github.io/token-registry/assets/3d55edb42b09a634f7f2f26756a02571de901a5b.png',
-    VOT3: 'https://vechain.github.io/token-registry/assets/17ff70aa1d898bc97ad690dbfad1a3b5643f7e0b.png',
-    veDelegate:
-        'https://vechain.github.io/token-registry/assets/1c641b86096d56bf13d49f38388accd6db8b8b2e.png',
-    USDGLO: 'https://raw.githubusercontent.com/vechain/app-hub/439fba60c80ba2521d435981102d88c4aec050d6/apps/org.glodollar.app/logo.png',
-};
 
-export const TOKEN_LOGO_COMPONENTS: Record<string, JSX.Element> = {
-    VET: <VETLogo />,
-    VTHO: <VTHOLogo />,
-    B3TR: <VeBetterIcon />,
-    VOT3: <VOT3Logo />,
-};
 
 export const VECHAIN_PRIVY_APP_ID = 'cm4wxxujb022fyujl7g0thb21';
 
