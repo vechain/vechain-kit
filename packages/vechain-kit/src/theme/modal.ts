de4a56e6ac5abb12ee2a6c4bda570d5017a828de
Dan Rusnac <rusnac14@gmail.com>
2025-12-19 10:41:14 +0100

    fix: border color on desktop

--- packages/vechain-kit/src/theme/modal.ts
+++ packages/vechain-kit/src/theme/modal.ts
@@ -15,7 +15,7 @@ const getModalVariants = (tokens: ThemeTokens) => ({
             backgroundColor: tokens.colors.background.modal,
             backdropFilter: tokens.effects.backdropFilter.modal,
             border: tokens.colors.border.modal,
-            borderBottomColor: 'transparent',
+            borderBottomColor: ['transparent', 'inherit'],
         },
         overlay: {
             backgroundColor: tokens.colors.background.overlay,
