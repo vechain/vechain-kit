976bdd1e2ddf95cf17bc9f6cccb4e2b995773e3a
Victor Emanuel <versaujob@gmail.com>
2025-12-29 14:06:19 +0000

    chore: remove unused URLs

--- packages/vechain-kit/src/constants/urls.ts
+++ packages/vechain-kit/src/constants/urls.ts
@@ -2,11 +2,7 @@
 
 // GitHub
 export const APP_HUB_GITHUB_API_BASE_URL = 'https://api.github.com';
-// Include the repo prefix so callers only append branch/sha + path (not owner/repo every time).
-export const APP_HUB_GITHUB_RAW_REPO_BASE_URL =
-    'https://raw.githubusercontent.com/vechain/app-hub';
-export const GITHUB_WEB_BASE_URL = 'https://github.com';
-
+// Include the repo prefix so callers only append branch/sha + path (not owner/repo every time)
 // Privy
 export const PRIVY_STATUS_BASE_URL = 'https://status.privy.io';
 export const PRIVY_AUTH_BASE_URL = 'https://auth.privy.io';
@@ -14,19 +10,13 @@ export const PRIVY_AUTH_BASE_URL = 'https://auth.privy.io';
 // VeChain / Infra
 export const VECHAIN_MAINNET_NODE_BASE_URL = 'https://mainnet.vechain.org';
 export const VECHAIN_TESTNET_NODE_BASE_URL = 'https://testnet.vechain.org';
-export const VECHAIN_ORG_BASE_URL = 'https://vechain.org';
 export const VECHAIN_KIT_WEBSITE_BASE_URL = 'https://vechainkit.vechain.org';
 export const VECHAIN_KIT_DOCS_BASE_URL = 'https://docs.vechainkit.vechain.org';
 
-export const VECHAIN_GATEWAY_PROXY_BASE_URL = 'https://api.gateway-proxy.vechain.org';
+export const VECHAIN_GATEWAY_PROXY_BASE_URL =
+    'https://api.gateway-proxy.vechain.org';
 export const VECHAIN_GATEWAY_PROXY_DEV_BASE_URL =
     'https://api.dev.gateway-proxy.vechain.org';
-
-export const VECHAIN_INDEXER_MAINNET_BASE_URL = 'https://indexer.mainnet.vechain.org';
-export const VECHAIN_INDEXER_TESTNET_BASE_URL = 'https://indexer.testnet.vechain.org';
-
-export const B3TR_INDEXER_MAINNET_BASE_URL = 'https://b3tr.mainnet.vechain.org';
-export const B3TR_INDEXER_TESTNET_BASE_URL = 'https://b3tr.testnet.vechain.org';
 
 // VNS (GraphQL)
 export const GRAPH_VET_BASE_URL = 'https://graph.vet';
@@ -36,16 +26,20 @@ export const VET_DOMAINS_BASE_URL = 'https://vet.domains';
 export const VET_DOMAINS_TESTNET_BASE_URL = 'https://testnet.vet.domains';
 
 // Delegator
-export const GENERIC_DELEGATOR_MAINNET_URL = 'https://mainnet.delegator.vechain.org';
-export const GENERIC_DELEGATOR_TESTNET_URL = 'https://testnet.delegator.vechain.org';
+export const GENERIC_DELEGATOR_MAINNET_URL =
+    'https://mainnet.delegator.vechain.org';
+export const GENERIC_DELEGATOR_TESTNET_URL =
+    'https://testnet.delegator.vechain.org';
 
 // Explorers
 export const VECHAIN_EXPLORER_BASE_URL = 'https://explore.vechain.org';
-export const VECHAIN_EXPLORER_TESTNET_BASE_URL = 'https://explore-testnet.vechain.org';
+export const VECHAIN_EXPLORER_TESTNET_BASE_URL =
+    'https://explore-testnet.vechain.org';
 export const VECHAINSTATS_BASE_URL = 'https://vechainstats.com';
 
 // Ecosystem / partners
-export const VEBETTERDAO_GOVERNANCE_BASE_URL = 'https://governance.vebetterdao.org';
+export const VEBETTERDAO_GOVERNANCE_BASE_URL =
+    'https://governance.vebetterdao.org';
 export const VECHAIN_ENERGY_SWAP_BASE_URL = 'https://swap.vechain.energy';
 export const VETRADE_BASE_URL = 'https://vetrade.vet';
 
@@ -58,13 +52,10 @@ export const EVEARN_BASE_URL = 'https://evearn.io';
 // VeChain docs
 export const VECHAIN_DOCS_BASE_URL = 'https://docs.vechain.org';
 
-// VeChain explorers / third parties
-export const VECHAIN_INSIGHT_TESTNET_BASE_URL = 'https://insight.vecha.in';
 
 // Token registry / assets
 export const VECHAIN_TOKEN_REGISTRY_ASSETS_BASE_URL =
     'https://vechain.github.io/token-registry/assets';
-export const CRYPTOLOGOS_BASE_URL = 'https://cryptologos.cc';
 export const COINMARKETCAP_STATIC_BASE_URL = 'https://s2.coinmarketcap.com';
 export const IMAGE_NOT_FOUND_URL =
     'https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png';
