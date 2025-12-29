368154636725dd72f151aae56118a2628ccae69d
Victor Emanuel <versaujob@gmail.com>
2025-12-29 13:43:44 +0000

    fix: update url constant file

--- packages/vechain-kit/src/constants/urls.ts
+++ packages/vechain-kit/src/constants/urls.ts
@@ -2,7 +2,9 @@
 
 // GitHub
 export const APP_HUB_GITHUB_API_BASE_URL = 'https://api.github.com';
-export const APP_HUB_GITHUB_RAW_BASE_URL = 'https://raw.githubusercontent.com';
+// Include the repo prefix so callers only append branch/sha + path (not owner/repo every time).
+export const APP_HUB_GITHUB_RAW_REPO_BASE_URL =
+    'https://raw.githubusercontent.com/vechain/app-hub';
 export const GITHUB_WEB_BASE_URL = 'https://github.com';
 
 // Privy
@@ -46,3 +48,40 @@ export const VECHAINSTATS_BASE_URL = 'https://vechainstats.com';
 export const VEBETTERDAO_GOVERNANCE_BASE_URL = 'https://governance.vebetterdao.org';
 export const VECHAIN_ENERGY_SWAP_BASE_URL = 'https://swap.vechain.energy';
 export const VETRADE_BASE_URL = 'https://vetrade.vet';
+
+// Ecosystem app websites
+export const CLEANIFY_APP_BASE_URL = 'https://app.cleanify.vet';
+export const GREENCART_BASE_URL = 'https://greencart.ai';
+export const MUGSHOT_BASE_URL = 'https://mugshot.vet';
+export const EVEARN_BASE_URL = 'https://evearn.io';
+
+// VeChain docs
+export const VECHAIN_DOCS_BASE_URL = 'https://docs.vechain.org';
+
+// VeChain explorers / third parties
+export const VECHAIN_INSIGHT_TESTNET_BASE_URL = 'https://insight.vecha.in';
+
+// Token registry / assets
+export const VECHAIN_TOKEN_REGISTRY_ASSETS_BASE_URL =
+    'https://vechain.github.io/token-registry/assets';
+export const CRYPTOLOGOS_BASE_URL = 'https://cryptologos.cc';
+export const COINMARKETCAP_STATIC_BASE_URL = 'https://s2.coinmarketcap.com';
+export const IMAGE_NOT_FOUND_URL =
+    'https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png';
+
+// Image / asset CDNs used in UI
+export const IBB_CO_BASE_URL = 'https://i.ibb.co';
+export const VECHAIN_KIT_DOCS_IMAGES_S3_BASE_URL =
+    'https://prod-vechainkit-docs-images-bucket.s3.eu-west-1.amazonaws.com';
+export const PRIVY_MINTLIFY_ASSETS_S3_BASE_URL =
+    'https://mintlify.s3.us-west-1.amazonaws.com';
+export const IMAGEDELIVERY_NET_BASE_URL = 'https://imagedelivery.net';
+
+// Social / sharing
+export const X_BASE_URL = 'https://x.com';
+export const TWITTER_BASE_URL = 'https://twitter.com';
+export const TELEGRAM_BASE_URL = 'https://telegram.me';
+export const WHATSAPP_BASE_URL = 'https://wa.me';
+
+// Storage / content addressing
+export const ARWEAVE_GATEWAY_BASE_URL = 'https://arweave.net';
