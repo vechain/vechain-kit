655844f5dd8ee91c33ba0e6eae680be28ffe2290
Victor Emanuel <versaujob@gmail.com>
2025-12-29 13:30:44 +0000

    fix: add url constant file

---
+++ packages/vechain-kit/src/constants/urls.ts
@@ -0,0 +1,48 @@
+// Base URLs only (scheme + host). No paths here by request.
+
+// GitHub
+export const APP_HUB_GITHUB_API_BASE_URL = 'https://api.github.com';
+export const APP_HUB_GITHUB_RAW_BASE_URL = 'https://raw.githubusercontent.com';
+export const GITHUB_WEB_BASE_URL = 'https://github.com';
+
+// Privy
+export const PRIVY_STATUS_BASE_URL = 'https://status.privy.io';
+export const PRIVY_AUTH_BASE_URL = 'https://auth.privy.io';
+
+// VeChain / Infra
+export const VECHAIN_MAINNET_NODE_BASE_URL = 'https://mainnet.vechain.org';
+export const VECHAIN_TESTNET_NODE_BASE_URL = 'https://testnet.vechain.org';
+export const VECHAIN_ORG_BASE_URL = 'https://vechain.org';
+export const VECHAIN_KIT_WEBSITE_BASE_URL = 'https://vechainkit.vechain.org';
+export const VECHAIN_KIT_DOCS_BASE_URL = 'https://docs.vechainkit.vechain.org';
+
+export const VECHAIN_GATEWAY_PROXY_BASE_URL = 'https://api.gateway-proxy.vechain.org';
+export const VECHAIN_GATEWAY_PROXY_DEV_BASE_URL =
+    'https://api.dev.gateway-proxy.vechain.org';
+
+export const VECHAIN_INDEXER_MAINNET_BASE_URL = 'https://indexer.mainnet.vechain.org';
+export const VECHAIN_INDEXER_TESTNET_BASE_URL = 'https://indexer.testnet.vechain.org';
+
+export const B3TR_INDEXER_MAINNET_BASE_URL = 'https://b3tr.mainnet.vechain.org';
+export const B3TR_INDEXER_TESTNET_BASE_URL = 'https://b3tr.testnet.vechain.org';
+
+// VNS (GraphQL)
+export const GRAPH_VET_BASE_URL = 'https://graph.vet';
+
+// VET Domains
+export const VET_DOMAINS_BASE_URL = 'https://vet.domains';
+export const VET_DOMAINS_TESTNET_BASE_URL = 'https://testnet.vet.domains';
+
+// Delegator
+export const GENERIC_DELEGATOR_MAINNET_URL = 'https://mainnet.delegator.vechain.org';
+export const GENERIC_DELEGATOR_TESTNET_URL = 'https://testnet.delegator.vechain.org';
+
+// Explorers
+export const VECHAIN_EXPLORER_BASE_URL = 'https://explore.vechain.org';
+export const VECHAIN_EXPLORER_TESTNET_BASE_URL = 'https://explore-testnet.vechain.org';
+export const VECHAINSTATS_BASE_URL = 'https://vechainstats.com';
+
+// Ecosystem / partners
+export const VEBETTERDAO_GOVERNANCE_BASE_URL = 'https://governance.vebetterdao.org';
+export const VECHAIN_ENERGY_SWAP_BASE_URL = 'https://swap.vechain.energy';
+export const VETRADE_BASE_URL = 'https://vetrade.vet';
