e471fbfdadc998ed191e1d8f361f85010fcb568a
Dan Rusnac <rusnac14@gmail.com>
2026-02-10 13:50:38 +0100

    fix: update addresses

--- examples/next-template/src/app/constants.ts
+++ examples/next-template/src/app/constants.ts
@@ -1,5 +1,5 @@
 export const b3trMainnetAddress = '0x5ef79995FE8a89e0812330E4378eB2660ceDe699';
-export const b3trTestnetAddress = '0xbf64cf86894Ee0877C4e7d03936e35Ee8D8b864F';
+export const b3trTestnetAddress = '0x95761346d18244bb91664181bf91193376197088';
 export const b3trAbi = [
     // Replace this with your actual transfer function ABI
     {
